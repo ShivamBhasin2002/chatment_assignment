803741904edc22c0c4562d95026a8c5aa95500af
Shivam Bhasin <bhasinshivam2002@gmail.com>
2022-06-23 01:23:17 +0530

    initial setup

---
+++ src/main/java/com/example/chatment_assignment/ChatmentAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatment_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatmentAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatmentAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatment_assignment/ChatmentAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatment_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatmentAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
